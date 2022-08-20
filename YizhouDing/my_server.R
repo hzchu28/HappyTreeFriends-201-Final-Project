aaa17a7e4dd8345602bbf1291b3f2e078c00dfb2
RitaHwang <hzchu28@gmail.com>
2022-08-19 19:39:17 -0700

    discount upload

--- YizhouDing/my_server.R
+++ YizhouDing/my_server.R
@@ -4,134 +4,91 @@ library("ggplot2")
 library("leaflet")
 library("shiny")
 library("plotly")
-setwd("~/Desktop/HappyTreeFriends-201-Final-Project/YizhouDing")
+
+
 source("my_ui.R")
 
-
 my_server <- function(input, output) {
-  output$pie19 <- renderPlot({
-    # 19_may_top10
-    players_df_19 <- read.csv("data/19_May_2022.csv")
-    players_df_19$Current_Players <- as.numeric(gsub(",", "", players_df_19$Current_Players))
+  source("steamdb.r")
+  output$line1 <- renderPlotly({
     
-    players_19_top10 <- players_df_19 %>%
-      head(10)
+    re_usefuldd <- filter(useful_dd, Release.date != 2002)
+    nofactordf <- data.frame(year_range = c(2006:2022), 
+                             mean = re_usefuldd$MeanDiscount, 
+                             max = re_usefuldd$MaxDiscount, 
+                             min = re_usefuldd$MinDiscount,
+                             stringsAsFactors = F)
     
-    players_19_top10_names <- players_19_top10 %>%
-      pull(Name)
-    
-    players_19_top10_curr_player <- players_19_top10 %>%
-      pull(Current_Players)
-    
-    pct_19 <- round(players_19_top10_curr_player/sum(players_19_top10_curr_player) * 100)
-    
-    lbls_19 <- paste(pct_19, players_19_top10_names, sep="% ")
-    
-    pie(players_19_top10_curr_player, lbls_19, main = "Current Player Pie Chart 19-May", col=rainbow(length(players_19_top10_names)))
-
+    selected_df <- reactive({
+      useful_dd_ <- filter(nofactordf, year_range <= input$year[2] & year_range >= input$year[1])
+      useful_dd_
     })
-  
-  output$pie18 <- renderPlot({
-    players_df_18 <- read.csv("data/18_May_2022.csv")
-    players_df_18$Current_Players <- as.numeric(gsub(",", "", players_df_18$Current_Players))
-
-    players_18_top10 <- players_df_18 %>%
-      head(10)
     
-    players_18_top10_names <- players_18_top10 %>%
-      pull(Name)
+    yearrange <- reactive({
+      selected_df()$year_range
+    })
     
-    players_18_top10_curr_player <- players_18_top10 %>%
-      pull(Current_Players)
+    MaxDis <- reactive({
+      selected_df()$max
+    })
     
-    pct_18 <- round(players_18_top10_curr_player/sum(players_18_top10_curr_player) * 100)
+    MinDis <- reactive({
+      selected_df()$min
+    })
     
-    lbls_18 <- paste(pct_18, players_18_top10_names, sep="% ")
+    MeanDis <- reactive({
+      selected_df()$mean
+    })
     
-    pie(players_18_top10_curr_player, lbls_18, main = "Current Player Pie Chart 18-May", col=rainbow(length(players_18_top10_names)))
-
+    fig1 <- plot_ly(selected_df(), x = ~yearrange(), y = ~MaxDis(), type = 'scatter', mode = 'lines',
+                    line = list(color = 'transparent'),
+                    showlegend = FALSE, name = 'Max.Discount') 
+    fig1 <- fig1 %>% add_trace(y = ~MinDis(), type = 'scatter', mode = 'lines',
+                               fill = 'tonexty', fillcolor='rgba(0,100,80,0.2)', line = list(color = 'transparent'),
+                               showlegend = FALSE, name = 'Min.Discount') 
+    fig1 <- fig1 %>% add_trace(x = ~yearrange(), y = ~MeanDis(), type = 'scatter', mode = 'lines',
+                               line = list(color='rgb(0,100,80)'),
+                               name = 'Average.Discount') 
+    fig1 <- fig1 %>% layout(title = "Average, Max and Min Discount in Release Year",
+                            paper_bgcolor='rgb(255,255,255)', plot_bgcolor='rgb(229,229,229)',
+                            xaxis = list(title = "Years",
+                                         gridcolor = 'rgb(255,255,255)',
+                                         showgrid = TRUE,
+                                         showline = FALSE,
+                                         showticklabels = TRUE,
+                                         tickcolor = 'rgb(127,127,127)',
+                                         ticks = 'outside',
+                                         zeroline = FALSE),
+                            yaxis = list(title = "Discount Percentage(%)",
+                                         gridcolor = 'rgb(255,255,255)',
+                                         showgrid = TRUE,
+                                         showline = FALSE,
+                                         showticklabels = TRUE,
+                                         tickcolor = 'rgb(127,127,127)',
+                                         ticks = 'outside',
+                                         zeroline = FALSE))
+    
+    fig1
   })
   
-  output$pie14 <- renderPlot({
-    players_df_14 <- read.csv("data/14_May_2022.csv")
-    players_df_14$Current_Players <- as.numeric(gsub(",", "", players_df_14$Current_Players))
-    
-    players_14_top10 <- players_df_14 %>%
-      head(10)
-    
-    
-    players_14_top10_names <- players_14_top10 %>%
-      pull(Game)
-    
-    players_14_top10_curr_player <- players_14_top10 %>%
-      pull(Current_Players)
-    
-    pct_14 <- round(players_14_top10_curr_player/sum(players_14_top10_curr_player) * 100)
-    
-    lbls_14 <- paste(pct_14, players_14_top10_names, sep="% ")
-    
-    pie(players_14_top10_curr_player, lbls_14, main = "Current Player Pie Chart 14-May", col=rainbow(length(players_14_top10_names)))
-    
+  output$table <- renderTable({
+    game_rate_discount_df <- data.frame(
+      Game = c("Counter-Strike: Global Offensive", "Terraria", "Call of Duty: Black Ops II", "Rust", "Grand Theft Auto V", "Sekiro: Shadows Die Twice"), 
+      Released.Year = c(2017, 2011, 2015, 2018, 2015, 2019),
+      Positive.Rate = c(96, 97, 84, 87, 85, 94), 
+      Historical.Greatest.Discount = c("NA-FreeGame", 80, 50, 100, 56, 35)
+    )
   })
   
-  output$pie10 <- renderPlot({
-    players_df_10 <- read.csv("data/10_May_2022.csv")
-    players_df_10$Current_Players <- as.numeric(gsub(",", "", players_df_10$Current.Players))
-    
-    players_10_top10 <- players_df_10 %>%
-      head(10)
-    
-    players_10_top10_names <- players_10_top10 %>%
-      pull(Name)
-    
-    players_10_top10_curr_player <- players_10_top10 %>%
-      pull(Current_Players)
-    
-    pct_10 <- round(players_10_top10_curr_player/sum(players_10_top10_curr_player) * 100)
-    
-    lbls_10 <- paste(pct_10, players_10_top10_names, sep="% ")
-    
-    pie(players_10_top10_curr_player, lbls_10, main = "Current Player Pie Chart 10-May", col=rainbow(length(players_10_top10_names)))
-    
-  })
-  output$pie9 <- renderPlot({
-    players_df_9 <- read.csv("data/9_May_2022.csv")
-    players_df_9$Current_Players <- as.numeric(gsub(",", "", players_df_9$Current.Players))
-    
-    players_9_top10 <- players_df_9 %>%
-      head(10)
-    
-    players_9_top10_names <- players_9_top10 %>%
-      pull(Name)
-    
-    players_9_top10_curr_player <- players_9_top10 %>%
-      pull(Current_Players)
-    
-    pct_9 <- round(players_9_top10_curr_player/sum(players_9_top10_curr_player) * 100)
-    
-    lbls_9 <- paste(pct_9, players_9_top10_names, sep="% ")
-    
-    pie(players_9_top10_curr_player, lbls_9, main = "Current Player Pie Chart 9-May", col=rainbow(length(players_9_top10_names)))
-  })
-  
-  output$pie8 <- renderPlot({
-    players_df_8 <- read.csv("data/8_May_2022.csv")
-    players_df_8$Current_Players <- as.numeric(gsub(",", "", players_df_8$Current.Players))
-    
-    players_8_top10 <- players_df_8 %>%
-      head(10)
-    
-    players_8_top10_names <- players_8_top10 %>%
-      pull(Name)
-    
-    players_8_top10_curr_player <- players_8_top10 %>%
-      pull(Current_Players)
-    
-    pct_8 <- round(players_8_top10_curr_player/sum(players_8_top10_curr_player) * 100)
-    
-    lbls_8 <- paste(pct_8, players_8_top10_names, sep="% ")
-    
-    pie(players_8_top10_curr_player, lbls_8, main = "Current Player Pie Chart 8-May", col=rainbow(length(players_8_top10_names)))
-    
+  output$line2 <- renderPlotly({
+    x <- c("<10%", "10%-20%", "20%-30%", "30%-40%", "40%-50%", "50%-60%", "60%-70%", "70%-80%", "80%-90%", "90%-100%")
+    meanfb <- c(mean10, mean20, mean30, mean40, mean50, mean60, mean70, mean80, mean90, mean100)
+    data1 <- data.frame(x, meanfb)
+    fig2 <- plot_ly(data1, x = ~x, y = ~meanfb, name = "Average Positive Feedback Rate", type = "scatter", mode = "lines", 
+                    line = list(color = "rgb(0, 100, 80)", width = 4))
+    fig2 <- fig2 %>% layout(title = "Discount vs. Positive Feedback Rate", 
+                            xaxis = list(title = "Discount"), 
+                            yaxis = list(title = "Positive Feedback Rate(%)"))
+    fig2
   })
 }
--- YizhouDing/my_ui.R
+++ YizhouDing/my_ui.R
@@ -1,116 +1,47 @@
-library("dplyr")
-library("knitr")
-library("ggplot2")
-library("leaflet")
-library("shiny")
-
-my_ui <- navbarPage(
-  #page 1: home
-  title = "Happy Tree Friends",
-  tabPanel(
-    "Home page",
-    #img(src = "https://specials-images.forbesimg.com/imageserve/1023761676/960x0.jpg?fit=scale"),
-    h3("Introduction:
-       Steam is the most popular video game digital distribution service
-       and storefront nowadays. The steam platform often has discounts, and
-       this report will aim to examine the reasons for them, as well as to
-       predict the trend of discounts. For the data part, we are going to
-       analyze 3 groups of data, to find out the relationship between the
-       number of current players, the hours users' played and the Steam game
-       discount price reduction.")
+# Define UI
+discount_ui <- fluidPage(
+  tags$head(
+    tags$style(HTML("
+                    @import url('https://fonts.google.com/specimen/Silkscreen');
+                    body{
+                    background-color: black;
+                    color: white;
+                    }
+                    h3{
+                    font-family: 'Silkscreen';
+                    }
+                    .shiny-input-container {
+                    color: #FFFFFF;
+                    }"))
   ),
-
-    #page 2: Rating
-    # tabPanel(
-    #   "Rating",
-    #   titlePanel("这里是图标题"),
-    #   mainPanel(
-    #     leafletOutput(outputId = "这里放图"),
-    #     tableOutput(outputId = "这里放图")
-    #   ),
-    #   h3("Steam allows players to rate games and all the rating data will
-    #   be generated as a percentage. Players could view the rating on each
-    #   game page so they could make a reasonable consideration when purchasing
-    #   it. A higher feedback rate does not only mean this game is fun to play,
-    #   other features are also considered, such as gaming animation and
-    #   production. This dataset shows the popularity of 15 games within a bar
-    #   chart. Positive feedback rate can be shown when the bar is higher, and
-    #   vice versa.")
-    # ),
-
-
-    #page 3: Peak player
-
-    tabPanel(
-      "Peak Player",
-      tabsetPanel(type = "tab",
-                  tabPanel("INTRO",
-                           h3("The second database collects 
-                           the names of 200 different games, 
-                           current players, peak players, and hours 
-                           played between May 8, 2022 and May 19,
-                           2022. The aim of this data is to discover 
-                           the regulation betweeneveryday and the reason 
-                           for the changes of the most popular games. In
-                           the 4 pie charts for data 2, shown the percentage 
-                           of the top 15 populargames in the whole Steam 
-                           game market for 6 days in May 2022. ")
-                  ),
-                  tabPanel("May-8-2022",
-                           plotOutput(outputId = "pie8")
-                  ),
-                  tabPanel("May-9-2022",
-                           plotOutput(outputId = "pie9")
-                  ),
-                  tabPanel("May-10-2022",
-                           plotOutput(outputId = "pie10")
-                  ),
-                  tabPanel("May-14-2022",
-                           plotOutput(outputId = "pie14")
-                  ),
-                  tabPanel("May-18-2022",
-                           plotOutput(outputId = "pie18")
-                  ),
-                  tabPanel("May-19-2022",
-                           plotOutput(outputId = "pie19")
-                  ),
-                  tabPanel("Summary",
-                           h3("While there are some differences in the column 
-                              names and the formats of the dataset, we are able 
-                              to do data cleaning and work with the data. 
-                              Through visualizing ana analyzing the dataset, 
-                              we find that top 10 games ranked by the numebr 
-                              of current players do not change a lot; however, 
-                              the percent changes slightly each day. By 
-                              analyzing the difference between the number of 
-                              current players by day, we are able to find 
-                              correlation between the number of current players 
-                              and factors influencing that, such as discount 
-                              on the game or update publishment.")
-                  )
+  titlePanel("Trendings showed by Discount Rate, Released Year, and Positive Feedback Rate"),
+  sidebarLayout(
+    sidebarPanel(
+      tags$style(".well {background-color: #000000;}"),
+      h3("Controls"), 
+      sliderInput(
+        inputId = "year", 
+        label = "Year Range", 
+        min = 2006, max = 2022, step = 1, 
+        value = c(2006, 2022)
       )
+    ), 
+    mainPanel(
+      plotlyOutput(outputId = "line1"), 
+      p("The line plots are good used to show trending of value changes and 
+        comparing values. This chart shows the relationship between the games’ average, min, and max 
+        discount rates and the games’ released year from 2006 to 2022. By filtering years, we see on average 
+        that with more recent release year, the chance a game to have high discount rate is low."), 
+      tableOutput(outputId = "table"),
+      p("With some collected data of current popular games, it seems hard to find out the relationship between 
+          discount and positive feedback rates. However, if we calculate mean collect data with a bigger sample size, 
+          there is a resonable relationship shown."),
+      plotlyOutput(outputId = "line2"), 
+      p("This chart shows the relationship between the 
+          games’ positive feedback rates change and discount rates. We found that on average in a big dataset of games, 
+          there is a trend that games discount less than 30% tend to have positive feedback rate greater than 85%, and those has dicount greater than
+          30% tend to have positive feedback rate less than 85 in a unstable way. The 
+          about games’ discount rates and users’ feedback rates.")
     )
-
-    #page 4: Discount
-    # tabPanel(
-    #   "Discount",
-    #   titlePanel("这里是图标题"),
-    #   mainPanel(
-    #     leafletOutput(outputId = "这里放图"),
-    #     tableOutput(outputId = "这里放图")
-    #   ),
-    #   h3()
-    # ),
-
-    #page 5: Summary
-    # tabPanel(
-    #   "DSummary",
-    #   titlePanel("这里是标题"),
-    #   mainPanel(
-    #     leafletOutput(outputId = "这里放图"),
-    #     tableOutput(outputId = "这里放图")
-    #   ),
-    #   h3("来个人写文字")
-    # )
-
+  )
 )
